cb754e56d93ebdb7af8d526825aa839f1276b79e
buildify-ug01 <operations+github_buildify-ug01@fusen.world>
2025-06-20 11:05:53 -0400

    UPDATE: src/pages/NotFoundPage.tsx

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,18 @@
+
+import { Link } from 'react-router-dom';
+
+const NotFoundPage = () => {
+  return (
+    <div className="not-found-page">
+      <div className="container">
+        <h1>404 - Page Not Found</h1>
+        <p>The page you are looking for doesn't exist or has been moved.</p>
+        <Link to="/">
+          <button className="primary-button">Back to Home</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
